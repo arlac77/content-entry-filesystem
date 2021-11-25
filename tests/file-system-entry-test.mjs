28f9fcfc3e704d61d743c41f4eb05c065a92c1af
Markus Felten <Markus.Felten@gmx.de>
2021-11-25 22:31:34 +0100

    fix: do not test deprecated api only current one

--- tests/file-system-entry-test.mjs
+++ tests/file-system-entry-test.mjs
@@ -1,10 +1,6 @@
 import test from "ava";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
-
 import { FileSystemEntry } from "content-entry-filesystem";
 
-const here = dirname(fileURLToPath(import.meta.url));
 
 test("fs entry create", async t => {
   const entry = new FileSystemEntry("somewhere", "/tmp");
@@ -23,31 +19,33 @@ test("fs entry create", async t => {
 });
 
 test("fs entry isExistent true", async t => {
-  const entry = new FileSystemEntry("file.txt", join(here, "fixtures"));
+  const entry = new FileSystemEntry(
+    "file.txt",
+    new URL("fixtures", import.meta.url).pathname
+  );
   t.true(await entry.isExistent);
 });
 
 test("fs entry isExistent false", async t => {
   const entry = new FileSystemEntry(
     "none_existing.txt",
-    join(here, "fixtures")
+    new URL("fixtures", import.meta.url).pathname
   );
   t.false(await entry.isExistent);
 });
 
 test("fs entry getString", async t => {
-  const entry = new FileSystemEntry("file.txt", join(here, "fixtures"));
-  t.is(await entry.getString(), "abc\n");
-  //t.is(await entry.string, "abc\n");
+  const entry = new FileSystemEntry("file.txt", new URL("fixtures", import.meta.url).pathname);
+  t.is(await entry.string, "abc\n");
 });
 
-test.skip("fs entry get string", async t => {
-  const entry = new FileSystemEntry("file.txt", join(here, "fixtures"));
+test("fs entry get string", async t => {
+  const entry = new FileSystemEntry("file.txt", new URL("fixtures", import.meta.url).pathname);
   t.is(await entry.string, "abc\n");
 });
 
 test("fs entry getReadStream", async t => {
-  const entry = new FileSystemEntry("file.txt", join(here, "fixtures"));
+  const entry = new FileSystemEntry("file.txt", new URL("fixtures", import.meta.url).pathname);
 
   let chunk;
   for await (chunk of await entry.getReadStream({ encoding: "utf-8" })) {
@@ -59,19 +57,21 @@ test("fs entry getReadStream", async t => {
 test("fs entry setString", async t => {
   const entry = new FileSystemEntry("file1.txt", "/tmp");
   await entry.setString("abc");
-  t.is(await entry.getString(), "abc");
+  t.is(await entry.string, "abc");
 });
 
 test("fs entry equals content true", async t => {
-  const a = new FileSystemEntry("file.txt", join(here, "fixtures"));
+  const a = new FileSystemEntry("file.txt", new URL("fixtures", import.meta.url).pathname);
   const b = new FileSystemEntry("file2.txt", "/tmp");
-  await b.setString(await a.getString());
+  await b.setString(await a.string);
   t.true(await a.equalsContent(b));
 });
 
 test("fs entry equals content false", async t => {
-  const a = new FileSystemEntry("file.txt", join(here, "fixtures"));
+  const a = new FileSystemEntry("file.txt", new URL("fixtures", import.meta.url).pathname);
   const b = new FileSystemEntry("file3.txt", "/tmp");
-  await b.setString("aaa");
+
+  //b.string = "aaa";
+  b.setString("aaa");
   t.false(await a.equalsContent(b));
 });
