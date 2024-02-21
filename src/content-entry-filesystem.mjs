d0cd5f117c33259d7f76220581c0855362dbe141
Markus Felten <Markus.Felten@gmx.de>
2024-02-21 14:41:15 +0000

    feat: provide types

--- src/content-entry-filesystem.mjs
+++ src/content-entry-filesystem.mjs
@@ -14,6 +14,7 @@ import { ContentEntry, StreamContentEntryMixin } from "content-entry";
  */
 export class FileSystemEntry extends StreamContentEntryMixin(ContentEntry) {
   constructor(name, baseDir) {
+    // @ts-ignore
     super(name);
     this.baseDir = baseDir;
   }
